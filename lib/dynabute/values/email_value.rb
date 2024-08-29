f6379fdd2299bd0aff3fcf7fd4af4c4707c29961
Vishal Mahto <vishal.mahto@softices.in>
2024-08-29 15:08:09 +0530

    first commit

---
+++ lib/dynabute/values/email_value.rb
@@ -0,0 +1,7 @@
+module Dynabute
+  module Values
+    class EmailValue < ActiveRecord::Base
+      include Dynabute::Values::Base
+    end
+  end
+end
---
+++ lib/dynabute/values/text_value.rb
@@ -0,0 +1,7 @@
+module Dynabute
+  module Values
+    class TextValue < ActiveRecord::Base
+      include Dynabute::Values::Base
+    end
+  end
+end
